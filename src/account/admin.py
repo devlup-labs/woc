36135103d1b13995e0cfc498211be335ee31b1f4
karan-modh <karanmodh71@gmail.com>
2018-08-26 16:08:01 +0530

    Added Accounts model

---
+++ src/account/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import StudentProfile, MentorProfile
+
+# Register your models here.
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    class Meta:
+        model = StudentProfile
+        fields = '__all__'
+
+@admin.register(MentorProfile)
+class MentorProifileAdmin(admin.ModelAdmin):
+    class Meta:
+        model = MentorProfile
+        fields = '__all__'
---
+++ src/account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+# Create your models here.
+
+class StudentProfile(models.Model):
+    roll = RegexValidator(r'^[BMP][0-9]{2}[A-Z]{2}[0-9]{3}')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roll_no = models.CharField(max_length=8, validators=[roll])
+
+class MentorProfile(models.Model):
+    roll = RegexValidator(r'^[BMP][0-9]{2}[A-Z]{2}[0-9]{3}')
+    name = models.CharField(max_length=50)
+    roll_no = models.CharField(max_length=8, validators=[roll])
