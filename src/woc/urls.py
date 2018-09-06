00daf5f107abea14402ce11ea264d1d476887bea
kavishgambhir <kavishgambhir21@gmail.com>
2018-09-07 03:50:48 +0530

    add API docs

--- src/woc/urls.py
+++ src/woc/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
+
+if settings.DEBUG:
+    from rest_framework.documentation import include_docs_urls
+    urlpatterns += [path('api/docs/', include_docs_urls(title='WoC API', public=False))]
