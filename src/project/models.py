861ae27759015db5fdb07ea616a010d5fffa2630
email.pulkitj@gmail.com <email.pulkitj@gmail.com>
2018-09-21 03:34:30 +0530

    Fixed Errors - Help text

--- src/project/models.py
+++ src/project/models.py
@@ -7,8 +7,8 @@ class Project(models.Model):
     description = models.TextField(help_text='Description of project')
     github_link = models.URLField(help_text='Github link of the project')
     # associations
-    students = models.ManyToManyField(
-        StudentProfile, through='Proposal', through_fields=('project', 'student'), help_text='Students working on the project')
+    students = models.ManyToManyField(StudentProfile, through='Proposal', through_fields=('project', 'student'),
+                                      help_text='Students working on the project')
     mentors = models.ManyToManyField(MentorProfile, help_text='Mentors of the project')
 
 
@@ -16,5 +16,5 @@ class Proposal(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, help_text='Proposal for the project')
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, help_text='Name of the student proposed')
     drive_link = models.URLField(help_text='Custom drive link for proposal')
-    file = models.FileField(upload_to='proposals',help_text='File link')
+    file = models.FileField(upload_to='proposals', help_text='File link')
     is_accepted = models.BooleanField(default=False, help_text='Accepted/Rejected')
