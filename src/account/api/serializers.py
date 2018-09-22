052b4e384aaa5437d860682faabc94c3922cfc8e
karan-modh <karanmodh71@gmail.com>
2018-09-22 23:21:40 +0530

    Removed UserViewSet

--- src/account/api/serializers.py
+++ src/account/api/serializers.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from account.models import StudentProfile, MentorProfile
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
+
+
 class StudentProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentProfile
@@ -15,9 +21,3 @@ class MentorProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = MentorProfile
         fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
