fca2323f98e0a398e66b2a4f4314b2d767db6e73
Anshul Ahuja <36476228+anshulahuja98@users.noreply.github.com>
2018-09-23 15:28:02 +0530

    Authentication check API

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from account.api.views import StudentProfileViewSet, MentorProfileViewSet, UserViewSet
+from .views import AuthenticationCheckAPIView
+from django.urls import path
 
 app_name = 'account'
 
@@ -7,4 +9,5 @@ router = DefaultRouter()
 router.register(r'student-profile', StudentProfileViewSet, base_name='student-profile')
 router.register(r'mentor-profile', MentorProfileViewSet, base_name='mentor-profile')
 router.register(r'user', UserViewSet, base_name='user')
-urlpatterns = router.urls
+
+urlpatterns = router.urls + path('auth-check/', AuthenticationCheckAPIView.as_view(), name='auth-check')
--- src/account/api/views.py
+++ src/account/api/views.py
@@ -3,6 +3,9 @@ from rest_framework import mixins
 from account.api.serializers import StudentProfileSerializer, MentorProfileSerializer, UserSerializer
 from account.models import StudentProfile, MentorProfile
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class StudentProfileViewSet(ModelViewSet):
@@ -18,3 +21,18 @@ class MentorProfileViewSet(ModelViewSet):
 class UserViewSet(mixins.UpdateModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.filter(is_active=True)
+
+
+class AuthenticationCheckAPIView(APIView):
+    """
+        get:
+        Returns the authentication status code and message for current request.
+    """
+
+    def get(self, request, *args, **kwargs):
+        authenticated = request.user.is_authenticated
+        data = {
+            'message': 'Authorized' if authenticated else 'Unauthorized'
+        }
+        status_code = status.HTTP_200_OK if authenticated else status.HTTP_401_UNAUTHORIZED
+        return Response(data, status=status_code)
