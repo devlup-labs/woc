09999471c99d73bd4b2f56517966585758245d19
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-25 20:16:05 +0530

    update Timeline

--- ui/src/components/Timeline.vue
+++ ui/src/components/Timeline.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card flat>
-        <v-card-title class="pb-0 pt-0" primary-title>
-          <div class="display-1 text-md-center">Timeline</div>
-        </v-card-title>
+    <v-card-title class="pb-0 pt-0" primary-title>
+      <div class="display-1 text-md-center">Timeline</div>
+    </v-card-title>
     <v-card-text>
       <v-stepper class="v-card--flat" v-model="e1" vertical>
         <template v-for="(item, key) in timeline">
@@ -13,7 +13,10 @@
             :step="key+1"
           >
             <v-flex>
-              <div @click="nextTimeline(key)" :style="{color: $vuetify.theme.primary}">{{item.title}}<v-spacer></v-spacer>{{item.startTime | humanize }}</div>
+              <div @click="nextTimeline(key)" :style="{color: $vuetify.theme.primary}">{{item.title}}
+                <v-spacer></v-spacer>
+                {{getEventTimestamp(item)}}
+              </div>
             </v-flex>
           </v-stepper-step>
           <v-stepper-content
@@ -35,135 +38,68 @@
       return {
         e1: 1,
         timeline: [
-          {// 1
-            startTime: new Date('2018-09-18T00:00:01'),
-            endTime: new Date('2018-09-19T23:59:59'),
+          {
+            startTime: new Date('2018-09-27T00:00:00'),
+            endTime: new Date('2018-09-27T00:00:00'),
+            title: 'Program Announced',
+            description: 'WoC is declared open and student community is informed about it.'
+          },
+          {
+            startTime: new Date('2018-09-27T00:00:00'),
+            endTime: new Date('2018-10-13T23:59:59'),
             title: 'Mentor Registrations Open',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'Potential mentors start registering on the platform and add their projects as well.'
           },
-          {// 2
-            startTime: new Date('2018-10-02T00:00:01'),
-            endTime: new Date('2018-10-02T23:59:59'),
-            title: 'Mentor Application Deadline',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+          {
+            startTime: new Date('2018-10-14T00:00:00'),
+            endTime: new Date('2018-10-14T00:00:00'),
+            title: 'Mentor Registration Ends and Projects Announced',
+            description: 'Students can have a look at the projects which is followed by the student registration.'
           },
-          {// 3
-            startTime: new Date('2018-10-04T00:00:01'),
-            endTime: new Date('2018-10-04T23:59:59'),
-            title: 'Mentor Announced with Projects',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+          {
+            startTime: new Date('2018-10-14T00:00:00'),
+            endTime: new Date('2018-10-14T00:00:00'),
+            title: 'Student Registration Begins',
+            description: 'Students start applying to the program and start preparing their proposals.'
           },
-          {// 4
-            startTime: new Date('2018-10-05T00:00:01'),
-            endTime: new Date('2018-10-15T23:59:59'),
-            title: 'Students Registration Open',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
-          },
-          {// 5
-            startTime: new Date('2018-10-14T00:00:01'),
-            endTime: new Date('2018-10-21T23:59:59'),
+          {
+            startTime: new Date('2018-10-14T00:00:00'),
+            endTime: new Date('2018-10-22T23:59:59'),
             title: 'Proposal Period',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'During this period students start preparing proposals for the projects and get it verified' +
+              ' by the mentors. At the end of this period, student registrations will close.'
           },
-          {// 6
-            startTime: new Date('2018-10-25T00:00:01'),
-            endTime: new Date('2018-10-25T23:59:59'),
-            title: 'Students Project Announced',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+          {
+            startTime: new Date('2018-10-25T00:00:00'),
+            endTime: new Date('2018-10-25T00:00:00'),
+            title: 'Student Projects Announced',
+            description: 'Students selected for the program are announced along with their projects.'
           },
-          {// 7
-            startTime: new Date('2018-10-26T00:00:01'),
-            endTime: new Date('2018-10-26T23:59:59'),
+          {
+            startTime: new Date('2018-10-26T00:00:00'),
+            endTime: new Date('2018-10-26T00:00:00'),
             title: 'Coding Begins',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'Students start coding their projects under the guidance of their mentors.'
           },
-          {// 8
-            startTime: new Date('2018-11-24T00:00:01'),
+          {
+            startTime: new Date('2018-11-24T00:00:00'),
             endTime: new Date('2018-11-26T23:59:59'),
             title: 'First Phase Evaluation',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'Mentors submit the report of first phase evaluation. Students satisfying this phase move ' +
+              'to the next phase.'
           },
-          {// 9
+          {
             startTime: new Date('2018-12-24T00:00:01'),
             endTime: new Date('2018-12-26T23:59:59'),
             title: 'Final Evaluation',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'Mentors submit the report of final phase evaluation. Students satisfying this phase are ' +
+              'considered to have successfully completed the program and are eligible for goodies.'
           },
-          {// 10
-            startTime: new Date('2019-01-01T00:00:01'),
-            endTime: new Date('2019-01-01T23:59:59'),
+          {
+            startTime: new Date('2019-01-01T00:00:00'),
+            endTime: new Date('2019-01-01T00:00:00'),
             title: 'Results Announced',
-            shortDescription: '',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-              'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown ' +
-              'printer took a galley of type and scrambled it to make a type specimen book. It has survived not ' +
-              'only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ' +
-              'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' +
-              'and more recently with desktop publishing software like Aldus PageMaker including versions of ' +
-              'Lorem Ipsum.'
+            description: 'Results are announced with the names of students who have successfully completed the program.'
           }
         ]
       }
@@ -175,11 +111,13 @@
         } else {
           this.e1 = n + 1
         }
-      }
-    },
-    filters: {
-      humanize (value) {
-        return value.toLocaleString('en-IN')
+      },
+      getEventTimestamp (event) {
+        if (event.startTime.toLocaleString() === event.endTime.toLocaleString()) {
+          return event.startTime.toDateString('en-in')
+        } else {
+          return `${event.startTime.toDateString('en-in')} to ${event.endTime.toDateString('en-in')}`
+        }
       }
     }
   }
