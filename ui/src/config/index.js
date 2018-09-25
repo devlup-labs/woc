cc3ef783173d711ca3d0596de451c38e0e4cf599
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-26 04:33:20 +0530

    remove config/index.js

--- ui/src/config/index.js
+++
@@ -1,4 +0,0 @@
-export const USE_HTTPS = process.env.NODE_ENV === 'production'
-export const BACKEND_API_HOST = '127.0.0.1'
-export const BACKEND_API_PORT = 8000
-export const BACKEND_API_ADDRESS = (USE_HTTPS ? 'https://' : 'http://') + BACKEND_API_HOST + (USE_HTTPS ? '' : ':' + BACKEND_API_PORT)
