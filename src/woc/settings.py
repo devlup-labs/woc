3a752feb070d86681f1d6f6b526ae3563db5246c
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-26 16:03:55 +0530

    set custom auth class to handle 403 -> 401

--- src/woc/settings.py
+++ src/woc/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'api.authentication.SessionAuthentication',
     )
 }
 
