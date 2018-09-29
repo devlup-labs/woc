bef113987934e1939aec410c2ccfb4954a797d28
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-29 15:31:38 +0530

    add delete confirm dialog

--- ui/src/components/Project.vue
+++ ui/src/components/Project.vue
@@ -4,11 +4,20 @@
       <h2>{{project.name}}</h2>
       <v-spacer></v-spacer>
       <div>
-        <v-icon class="mx-2" v-if="mentor" @click="remove">fa-trash</v-icon>
+        <v-icon class="mx-2" v-if="mentor" @click="dialog = true">fa-trash</v-icon>
         <v-icon class="mx-2" v-if="mentor">fa-pencil</v-icon>
         <a :href="project.github_link" target="_blank" class="dashline">
           <v-icon class="mx-2">fa-github</v-icon>
         </a>
+        <v-dialog v-model="dialog" max-width="320">
+          <v-card>
+          <v-card-text>Are you sure you want to delete?</v-card-text>
+          <v-card-actions>
+            <v-btn @click="remove">Yes</v-btn>
+            <v-btn @click="dialog = false">No</v-btn>
+          </v-card-actions>
+        </v-card>
+        </v-dialog>
       </div>
     </v-layout>
     <v-layout row>
@@ -35,6 +44,11 @@
   export default {
     name: 'Project',
     props: ['project', 'mentor'],
+    data () {
+      return {
+        dialog: false
+      }
+    },
     computed: {
       chips () {
         return this.project.technologies
@@ -46,6 +60,7 @@
           this.$store.dispatch('projectList/removeProjectById', this.project.id)
           this.$store.dispatch('messages/showMessage', {message: `Project ${this.project.name} was deleted!`, color: 'success'}, {root: true})
         }).catch(() => this.$store.dispatch('messages/showMessage', {message: 'Error deleting project!', color: 'error'}, {root: true}))
+        this.dialog = false
       }
     }
   }
