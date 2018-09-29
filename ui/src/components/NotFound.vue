b5acfae693f579ab586d71472c8f655ab4de5155
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-30 01:23:30 +0530

    add 404 page

---
+++ ui/src/components/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fill-height>
+    <v-layout row align-center justify-center>
+      <v-layout xs12 column>
+        <v-icon style="font-size: 24rem">fa-unlink</v-icon>
+        <h1 class="display-3 text-xs-center blue--text">Page Not Found</h1>
+        <p class="display-1 text-xs-center blue--text">Oops! You seem lost...</p>
+        <v-layout align-center justify-center><v-btn :to="{name: 'Home'}">Go to Home</v-btn></v-layout>
+      </v-layout>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home'
 import Login from '../components/Login'
 import ProjectsList from '../components/ProjectsList'
 import HowItWorks from '../components/HowItWorks'
+import NotFound from '../components/NotFound'
 import Help from '../components/Help'
 import Dashboard from '../components/Dashboard'
 
@@ -18,7 +19,9 @@ const router = new Router({
     {path: '/projects', name: 'Projects', component: ProjectsList},
     {path: '/how-it-works', name: 'HowItWorks', component: HowItWorks},
     {path: '/help', name: 'Help', component: Help},
-    {path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: {requiresAuth: true}}
+    {path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: {requiresAuth: true}},
+    { path: '/not-found', component: NotFound },
+    { path: '*', redirect: '/not-found' }
   ]
 })
 
