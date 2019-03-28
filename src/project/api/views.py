bf6c19535b9279059c296d812e0d6783f498dc3e
Ajat Prabha <ajatprabha@users.noreply.github.com>
2019-03-28 23:24:40 +0530

    remove unused import

--- src/project/api/views.py
+++ src/project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from account.api.permissions import IsApprovedMentor, IsStudent
+from account.api.permissions import IsApprovedMentor
 from project.api.serializers import ProjectSerializer, StudentProposalSerializer, StudentProposalApproveSerializer
 from project.models import StudentProposal, Project
 
