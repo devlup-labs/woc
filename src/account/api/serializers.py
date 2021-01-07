bcaa7ee672c48f392478bb6685ce5f5c525d2b63
Soham <42870378+killbotXD@users.noreply.github.com>
2021-01-08 01:31:40 +0530

    fix error that i thought was there but was not

--- src/account/api/serializers.py
+++ src/account/api/serializers.py
@@ -6,7 +6,7 @@ from account.models import StudentProfile, MentorProfile
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'github', 'linked_in')
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 
 class StudentProfileSerializer(serializers.ModelSerializer):
