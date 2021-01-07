2901dc0b6f4df551ca0abfbe467fabda722064a0
Soham <42870378+killbotXD@users.noreply.github.com>
2021-01-08 00:03:55 +0530

    add github to mentor serializer

--- src/account/api/serializers.py
+++ src/account/api/serializers.py
@@ -30,7 +30,7 @@ class MentorsListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MentorProfile
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'about_me')
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'about_me','github')
 
 
 class StudentsListSerializer(serializers.ModelSerializer):
