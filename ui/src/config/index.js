bee3daa06e3197e863adb49728133a40a0520ebb
Akky <abhiroop.art@gmail.com>
2023-07-17 17:13:57 +0530

    Added student proposal section

--- ui/src/config/index.js
+++ ui/src/config/index.js
@@ -3,4 +3,4 @@
 // export const BACKEND_API_PORT = 8000
 // export const BACKEND_API_ADDRESS = (USE_HTTPS ? 'https://' : 'http://') + BACKEND_API_HOST + (USE_HTTPS ? '' : ':' + BACKEND_API_PORT)
 
-export const BACKEND_API_ADDRESS = 'https://soc.devluplabs.tech'
+export const BACKEND_API_ADDRESS = 'http://localhost:8000'
